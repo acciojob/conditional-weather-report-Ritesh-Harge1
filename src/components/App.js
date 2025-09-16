3afe0ee901aa2cabbc52bf72b1de863224bf71f0
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-16 09:01:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
+import WeatherDisplay from "./WeatherDisplay";
 
 function App() {
-  // Hardcoded weather input
-  const [weather] = useState({
-    temperature: 25,
-    conditions: "Sunny",
-  });
-
-  // Inline style for temperature
-  const tempStyle = {
-    color: weather.temperature > 20 ? "red" : "blue",
-  };
+  const [weather] = useState({ temperature: 25, conditions: "Sunny" });
 
   return (
     <div>
-      {/* First p tag for temperature */}
-      <p>
-        Temperature: <span style={tempStyle}>{weather.temperature}</span>
-      </p>
-
-      {/* Second p tag for conditions */}
-      <p>Conditions: {weather.conditions}</p>
+      <WeatherDisplay weather={weather} />
     </div>
   );
 }
 
 export default App;
+
